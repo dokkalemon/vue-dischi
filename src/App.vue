dc612711d41dbefc23a41b8e8054f5fe3f663dbb
dokkalemon <dottori.roberto@gmail..com>
2021-11-25 15:48:38 +0100

    import Api items

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
-  <Header />
+  <div class="app">
+    <!-- Header -->
+    <Header />
+
+    <!-- Main -->
+    <Main />
+
+  </div>
+
 </template>
 
 <script>
 import Header from '@/components/Header.vue'
+import Main from '@/components/Main.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
+    Main,
   }
 }
 </script>
---
+++ src/components/Main.vue
@@ -0,0 +1,47 @@
+<template>
+  <main class="full-width debug">
+
+  </main>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'Main',
+
+    data() {
+        return {
+            albumList: null,
+        }
+    },
+
+    created() {
+        this.getAlbumList()
+    },
+
+    methods: {
+        getAlbumList() {
+            axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then(result => {
+                this.albumList = result.data.response;
+            })
+            .catch(err => console.log(err))
+        }
+    }
+
+    
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/styles/vars';
+
+main {
+    height: calc(100vh - 60px);
+}
+
+
+
+
+</style>
