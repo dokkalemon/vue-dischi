af85b807f39a206cb4a17e01259eec2f8d32b53d
dokkalemon <dottori.roberto@gmail..com>
2021-11-26 14:41:29 +0100

    add Select in Header Components

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,17 @@
 <template>
    <header class="px-15">
+       <div class="logo">
        <img src="@/assets/logo-spotify.png" alt="">
+       </div>
+       <div class="select">
+           <select name="" id="">
+               <option value="">All</option>
+               <option value="">Rock</option>
+               <option value="">Pop</option>
+               <option value="">Jazz</option>
+               <option value="">Metal</option>
+           </select>
+       </div>
    </header>
 </template>
 
@@ -17,6 +28,30 @@ header {
     height: 60px;
     padding: 5px;
     background-color: $primary-color;
+    display: flex;
+
+    .logo {
+        border: 1px solid red;
+        height: 100%;
+    }
+    .select {
+        border: 1px solid red;
+        flex-grow: 1;
+        display: flex;
+        justify-content: flex-end;
+        padding: 10px;
+        select {
+            background-color: transparent;
+            border: none;
+            border-bottom: 1px solid white;
+            width: 200px;
+            font-size: 20px;
+        }
+        option {
+            background-color: $primary-color;
+            color: white;
+        }
+    }
 }
 
 
